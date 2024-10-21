582ad23015c189f8ca102eb1ea4fc6db1411fbf3
Mst. Aroni Begum <aroni.begum@bracits.com>
2024-10-21 16:49:06 +0600

    Test Runner Update

--- src/test/java/Test/CreateSlotTestRunner.java
+++ src/test/java/Test/CreateSlotTestRunner.java
@@ -1,8 +1,28 @@
 package Test;
+import net.bytebuddy.build.Plugin;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 import pages.Login;
 import pages.CreateSlot;
 import setup.Setup;
 
 public class CreateSlotTestRunner extends Setup {
+    Login objLogin;
 
+    @BeforeTest
+    public void visitUrl(){
+        driver.get("https://careerhub-frontend.bracits.com/");
+    }
+
+    @Test
+    public void doLogin(){
+        objLogin = new Login();
+
+        objLogin.login("sdp_c", "123456");
+        String expectedTitle = "Today's Summary";
+        String pageHeader = driver.findElement(By.tagName("h2")).getText();
+        Assert.assertEquals(pageHeader, expectedTitle, "Not matched");
+    }
 }
